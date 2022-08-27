a84cb1030e62eb14661cfab14800bf47f619cad8
MUK-Dev <manian1221@gmail.com>
2022-08-27 11:23:13 +0500

    Debugging vercel build issue

--- src/Components/UI/Header/NavigationLink.jsx
+++ src/Components/UI/Header/NavigationLink.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavigationLink = ({ path, title }) => {
+  console.log('NavigationLink', path, title);
   return (
     <NavLink
       to={path}
